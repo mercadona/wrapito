e063e9d33651a21be41af483e3dc44e8f3f4596c
Perizote <sergio.perizote@gmail.com>
2020-02-17 16:28:09 +0100

    Include checking missing mocks for a given request

--- src/assertions.js
+++ src/assertions.js
@@ -1,23 +1,41 @@
 import diff from 'jest-diff'
 
-import { getDeepUtilizedResponses } from './notUtilizedResponses'
+import { getDeepUtilizedResponses, getRequestsMissingResponse } from './notUtilizedResponses'
 
 const assertions = {
   toMatchNetworkRequests (mockedResponses) {
-    const deepUtilizedResponses = getDeepUtilizedResponses()
+    const requestsMissingResponse = getRequestsMissingResponse()
+    const thereAreRequestsMissingResponse = requestsMissingResponse.length > 0
 
-    const responsesDiff = diff(deepUtilizedResponses, mockedResponses)
-    const pass = responsesDiff.includes('Compared values have no visual difference.')
-    const message = () => {
-      if (pass) { return }
-
-      return 'Expected to match the network requests but found mocked responses not being used:' +
-      '\n\n' +
-      responsesDiff
+    if (thereAreRequestsMissingResponse) {
+      return createJestMatcher(
+        [],
+        requestsMissingResponse,
+        'Expected mocked responses to match the network requests but there are requests missing a mocked response:'
+      )
     }
 
-    return { message, pass }
+    const deepUtilizedResponses = getDeepUtilizedResponses()
+    return createJestMatcher(
+      deepUtilizedResponses,
+      mockedResponses,
+      'Expected mocked responses to match the network requests but found mocked responses not being used:'
+    )
   },
+}
+
+const createJestMatcher = (expected, received, errorMessage) => {
+  const responsesDiff = diff(expected, received)
+  const pass = responsesDiff.includes('Compared values have no visual difference.')
+  const message = () => {
+    if (pass) { return }
+
+    return errorMessage +
+    '\n\n' +
+    responsesDiff
+  }
+
+  return { message, pass }
 }
 
 export { assertions }
