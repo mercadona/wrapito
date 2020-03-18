f3289d2179ad487d71f4776b8394c00c4ef3ae68
Enrique Palenque <epalenque83@gmail.com>
2020-03-18 15:48:12 +0100

    remove withStore

--- src/wrap.js
+++ src/wrap.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 
-import { mockStore } from './mockStore'
 import { MockRouter } from './mockRouter'
 import { mockFetch } from './mockFetch'
 import { getMocksConfig } from './config'
@@ -18,11 +16,10 @@ const wrap = options => {
     withProps: props => wrap({ ...options, props }),
     withPortalAt: portalRootId => wrap({ ...options, portalRootId, hasPortal: true }),
     withRouter: routing => wrap({ ...options, routing, hasRouter: true }),
-    withStore: store => wrap({ ...options, store, hasStore: true }),
     withMocks: responses => wrap({ ...options, responses, hasMocks: true }),
     atPath: path => wrap({ ...options, path, hasPath: true }),
     mount: () => {
-      const { hasMocks, responses, hasRouter, hasStore, hasPortal, portalRootId, path, hasPath } = options
+      const { hasMocks, responses, hasRouter, hasPortal, portalRootId, path, hasPath } = options
 
       if (hasMocks) {
         mockFetch(responses)
@@ -30,14 +27,6 @@ const wrap = options => {
 
       if (hasPortal) {
         setupPortal(portalRootId)
-      }
-
-      if (hasRouter && hasStore) {
-        return mountWithStoreAndRouter(options)
-      }
-
-      if (hasStore) {
-        return mountWithStore(options)
       }
 
       if (hasRouter) {
@@ -66,18 +55,5 @@ const mount = ({ Component, props }) => getMocksConfig().mount(<Component { ...p
 const mountWithRouter = ({ Component, props: componentProps, routing }) => getMocksConfig().mount(
   <MockRouter { ...{ Component, routing, componentProps } } />
 )
-
-const mountWithStore = ({ Component, props, store }) => getMocksConfig().mount(
-  <Provider store={ mockStore(store) }>
-    <Component { ...props } />
-  </Provider>
-)
-
-const mountWithStoreAndRouter = ({ Component, props: componentProps, store, routing }) => getMocksConfig().mount(
-  <Provider store={ mockStore(store) }>
-    <MockRouter { ...{ Component, routing, componentProps } } />
-  </Provider>
-)
-
 
 export { wrap }
