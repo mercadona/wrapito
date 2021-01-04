302e09c551574726978a8b09cdfa33f467fe68d7
Salva Carsi <salvavcarsi@gmail.com>
2021-01-04 17:17:56 +0100

    [Added] To have been fetched with (#35)
    * [Added] toHaveBeenFetchedWith checks the method
    
    * [Changed] toHaveBeenRequestedWith should allow to leave the method empty
    
    * [Changed] improve method name
    
    * [Added] missing test for toHaveBeenFetchedWith

--- src/globalFetchAssertions.js
+++ src/globalFetchAssertions.js
@@ -1,15 +1,41 @@
-const hasBeenFetched = (path) => {
-  return fetch.mock.calls.find(([ mockedPath ]) => mockedPath === path)
+const hasBeenFetched = path => {
+  return fetch.mock.calls.find(([mockedPath]) => mockedPath === path)
 }
+
+const findRequestByPath = path =>
+  fetch.mock.calls.find(([mockedPath]) => mockedPath === path)
+
+const getRequestMethod = request => request[1].method
+
+const noMatchingMethod = (options, targetRequestMethod) => options?.method && targetRequestMethod !== options.method
 
 const globalFetchAssertions = {
   toHaveBeenFetched(path) {
-
     if (hasBeenFetched(path)) {
       return { message: () => undefined, pass: true }
     }
 
     return { pass: false, message: () => `${path} ain't got called` }
+  },
+
+  toHaveBeenFetchedWith(path, options) {
+    const targetRequest = findRequestByPath(path)
+
+    if (!targetRequest) {
+      return { pass: false, message: () => `${path} ain't got called` }
+    }
+
+    const targetRequestMethod = getRequestMethod(targetRequest)
+
+    if (noMatchingMethod(options, targetRequestMethod)) {
+      return {
+        pass: false,
+        message: () =>
+          `fetch method does not match, expected ${options.method} received ${targetRequestMethod}`,
+      }
+    }
+
+    return { message: () => undefined, pass: true }
   },
 }
 
