656560987fe9eb6b985471a873f2058e6496d41f
Salva Carsi Moreno <scarsi@mercadona.es>
2021-01-04 15:47:44 +0100

    [Added] toHaveBeenFetchedWith checks the method

--- src/globalFetchAssertions.js
+++ src/globalFetchAssertions.js
@@ -1,15 +1,39 @@
-const hasBeenFetched = (path) => {
-  return fetch.mock.calls.find(([ mockedPath ]) => mockedPath === path)
+const hasBeenFetched = path => {
+  return fetch.mock.calls.find(([mockedPath]) => mockedPath === path)
 }
+
+const findRequestByPath = path =>
+  fetch.mock.calls.find(([mockedPath]) => mockedPath === path)
+
+const getRequestMethod = request => request[1].method
 
 const globalFetchAssertions = {
   toHaveBeenFetched(path) {
-
     if (hasBeenFetched(path)) {
       return { message: () => undefined, pass: true }
     }
 
     return { pass: false, message: () => `${path} ain't got called` }
+  },
+
+  toHaveBeenFetchedWith(path, options) {
+    const targetRequest = findRequestByPath(path)
+
+    if (!targetRequest) {
+      return { pass: false, message: () => `${path} ain't got called` }
+    }
+
+    const targetRequestMethod = getRequestMethod(targetRequest)
+
+    if (targetRequestMethod !== options.method) {
+      return {
+        pass: false,
+        message: () =>
+          `fetch method does not match, expected ${options.method} received ${targetRequestMethod}`,
+      }
+    }
+
+    return { message: () => undefined, pass: true }
   },
 }
 
---
+++ tests/jest-extensions/globalFetchWrapper.test.js
@@ -0,0 +1,64 @@
+import { globalFetchAssertions } from '../../src'
+
+expect.extend(globalFetchAssertions)
+
+it('should check that the path has been called', async () => {
+  const path = '/some/path/'
+  const expectedPath = '/some/path/'
+  await fetch(path)
+
+  const { message } = globalFetchAssertions.toHaveBeenFetched(expectedPath)
+
+  expect(message()).toBeUndefined()
+  expect(expectedPath).toHaveBeenFetched()
+})
+
+it('should check that the path has not been called', async () => {
+  const path = '/some/unknown'
+
+  await fetch('/some/path')
+  const { message } = globalFetchAssertions.toHaveBeenFetched(path)
+
+  expect(message()).toBe("/some/unknown ain't got called")
+  expect(path).not.toHaveBeenFetched()
+})
+
+describe('toHaveBeenFetchedWith', () => {
+  it('should check that the path has been called with the supplied method', async () => {
+    const path = '/some/path/'
+    const expectedPath = '/some/path/'
+    await fetch(path, { method: 'POST' })
+
+    const { message } = globalFetchAssertions.toHaveBeenFetchedWith(
+      expectedPath,
+      {
+        method: 'POST',
+      },
+    )
+
+    expect(message()).toBeUndefined()
+    expect(expectedPath).toHaveBeenFetchedWith({
+      method: 'POST',
+    })
+  })
+
+  it('should check that the path has not been called with the supplied method', async () => {
+    const path = '/some/path/'
+    const expectedPath = '/some/path/'
+    await fetch(path, { method: 'PUT' })
+
+    const { message } = globalFetchAssertions.toHaveBeenFetchedWith(
+      expectedPath,
+      {
+        method: 'POST',
+      },
+    )
+
+    expect(message()).toBe(
+      'fetch method does not match, expected POST received PUT',
+    )
+    expect(expectedPath).not.toHaveBeenFetchedWith({
+      method: 'POST',
+    })
+  })
+})
