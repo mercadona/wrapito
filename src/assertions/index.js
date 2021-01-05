69c508adc47b09a4e293442ca6a9a48549493f3e
Salva Carsi <salvavcarsi@gmail.com>
2021-01-05 14:51:43 +0100

    [Changed] organize assertions in its own directory (#37)
    * [Changed] rename jest-extensions folder to assertions
    
    * [Changed] move assertions file inside the assertions folder
    
    * [Changed] rename assertions.js to to-match-network-request.js
    
    * [Changed] keep project naming consistency for js file
    
    * [Changed] export toHaveBeenFetchedWith as part of the assertions
    
    * [Changed] rename globalFetchAssertions.js to toHaveBeenFetchedWith.js
    
    * [Changed] rename tests/jest-extensions folder to tests/assertions
    
    * [Changed] rename globalFetchWrapper.test to toHaveBeenFetchedWith.test
    
    * [Changed] rename assertions.test to toMatchNetworkRequests.test

---
+++ src/assertions/index.js
@@ -0,0 +1,9 @@
+import { toMatchNetworkRequests } from './toMatchNetworkRequests'
+import { toHaveBeenFetchedWith } from './toHaveBeenFetchedWith'
+
+const assertions = {
+  toMatchNetworkRequests,
+  toHaveBeenFetchedWith,
+}
+
+export { assertions }
