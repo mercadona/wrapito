b282f7917d1bd6484e90d3d53a09e258bc62e70a
Salva Carsi <salvavcarsi@gmail.com>
2021-01-08 11:19:35 +0100

    [Changed] Adapt code to use the Request object (#42)
    * [Changed] adapt code to work with the request object, getting the url and the body
    
    * [Changed] adapt code to Request object for the body extraction
    
    * [Changed] adapt code to use request object for extracting the method
    
    * [Changed] improve constants namings
    
    * [Removed] not necessary flat function call

--- src/assertions/toHaveBeenFetchedWith.js
+++ src/assertions/toHaveBeenFetchedWith.js
@@ -1,57 +1,66 @@
 const findRequestsByPath = path =>
-  fetch.mock.calls.filter(([mockedPath]) => mockedPath.includes(path))
+  fetch.mock.calls.filter(call => call[0].url.includes(path))
 
 const getRequestsMethods = requests =>
-  requests.map(request => request[1]?.method)
+  requests.map(request => request[0]?.method)
 
-const getRequestsBodies = requests => requests.map(request => request[1]?.body)
+const getRequestsBodies = async requests =>
+  Promise.all(
+    requests.map(request =>
+      request[0]
+        .clone()
+        .json()
+        .catch(_ => undefined),
+    ),
+  )
 
-const methodDoesNotMatch = (expectedMethod, targetRequestsMethods) =>
-  expectedMethod && !targetRequestsMethods.includes(expectedMethod)
+const methodDoesNotMatch = (expectedMethod, receivedRequestsMethods) =>
+  expectedMethod && !receivedRequestsMethods.includes(expectedMethod)
 
-const bodyDoesNotMatch = (expectedBody, targetRequestsBodies) => {
+const bodyDoesNotMatch = (expectedBody, receivedRequestsBodies) => {
   if (!expectedBody) return false
 
-  const expectedBodyEntries = JSON.stringify(
-    Object.entries(expectedBody).sort(),
-  )
-  const targetRequestBodyEntries = targetRequestsBodies.map(request =>
-    JSON.stringify(Object.entries(request).sort()),
+  const comparableExpectedBody = Object.entries(expectedBody)
+    .sort()
+    .join()
+
+  const comparableTargetRequestsBodies = receivedRequestsBodies.map(request =>
+    Object.entries(request).sort().join(),
   )
 
-  return !targetRequestBodyEntries.includes(expectedBodyEntries)
+  return !comparableTargetRequestsBodies.includes(comparableExpectedBody)
 }
 
 const empty = requests => requests.length === 0
 
-const toHaveBeenFetchedWith = (path, options) => {
+const toHaveBeenFetchedWith = async (path, options) => {
   const targetRequests = findRequestsByPath(path)
 
   if (empty(targetRequests)) {
     return { pass: false, message: () => `${path} ain't got called` }
   }
 
-  const targetRequestsMethods = getRequestsMethods(targetRequests)
+  const receivedRequestsMethods = getRequestsMethods(targetRequests)
   const expectedMethod = options?.method
 
-  if (methodDoesNotMatch(expectedMethod, targetRequestsMethods)) {
+  if (methodDoesNotMatch(expectedMethod, receivedRequestsMethods)) {
     return {
       pass: false,
       message: () =>
-        `Fetch method does not match, expected ${options.method} received ${targetRequestsMethods}`,
+        `Fetch method does not match, expected ${expectedMethod} received ${receivedRequestsMethods}`,
     }
   }
 
-  const targetRequestsBodies = getRequestsBodies(targetRequests)
+  const receivedRequestsBodies = await getRequestsBodies(targetRequests)
   const expectedBody = options?.body
 
-  if (bodyDoesNotMatch(expectedBody, targetRequestsBodies)) {
+  if (bodyDoesNotMatch(expectedBody, receivedRequestsBodies)) {
     return {
       pass: false,
       message: () =>
         `Fetch body does not match, expected ${JSON.stringify(
-          options.body,
-        )} received ${JSON.stringify(targetRequestsBodies)}`,
+          expectedBody,
+        )} received ${JSON.stringify(receivedRequestsBodies)}`,
     }
   }
 
