a4268a314b1ea1a783e4f07c7bfca256fe7351ac
Juan Diego <juandiego.mbr@gmail.com>
2021-03-30 09:21:45 +0200

    [Added] Delay to responses

--- tests/withNetwork.test.js
+++ tests/withNetwork.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, wait } from '@testing-library/react'
 import { wrap, configure } from '../src/index'
 
 import { MyComponentWithNetwork } from './components.mock'
@@ -43,4 +43,29 @@ it('should have network without responses', async () => {
   wrap(MyComponentWithNetwork).withNetwork().mount()
 
   expect(await screen.findByText('SUCCESS')).toBeInTheDocument()
+})
+
+it('should not resolve a request with delay', async () => {
+  configure({ mount: render })
+  jest.useFakeTimers()
+  wrap(MyComponentWithNetwork)
+    .withNetwork(
+      {
+        path: '/path/with/response/',
+        host: 'my-host',
+        responseBody: '15',
+        delay: 500,
+      },
+    )
+    .mount()
+
+  await wait()
+  const delayedResponseBeforeTime = screen.queryByText('15')
+  jest.advanceTimersByTime(500)
+  await wait()
+
+  expect(delayedResponseBeforeTime).not.toBeInTheDocument()
+  const delayedResponseAfterTime = screen.getByText('15')
+  expect(delayedResponseAfterTime).toBeInTheDocument()
+  jest.useRealTimers()
 })
