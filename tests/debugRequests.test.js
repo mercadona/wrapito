34650973adc2bdcd630aa94122183298b7563653
Salva Carsi Moreno <scarsi@mercadona.es>
2021-05-18 13:27:58 +0200

    remove await dependency

--- tests/debugRequests.test.js
+++ tests/debugRequests.test.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { render, wait, cleanup } from '@testing-library/react'
+import React, { useEffect, useState } from 'react'
+import { render, cleanup, screen } from '@testing-library/react'
 import { wrap, configure, highlightNotUtilizedResponses } from '../src/index'
 import { MyComponentMakingHttpCalls } from './components.mock'
 
@@ -10,91 +10,109 @@ afterEach(() => {
   console.warn.mockRestore()
 })
 
-const DummyComponent = () => {
+const GreetingComponent = () => {
+  const [name, setName] = useState('')
+
   useEffect(() => {
     async function fetchData() {
-      await fetch(new Request('my-host/request1'))
+      const response = await fetch(
+        new Request('my-host/request1', {
+          method: 'POST',
+          body: JSON.stringify({ id: 1 }),
+        }),
+      )
+      const data = await response.json()
+      setName(data.name)
 
       await fetch(
         new Request('my-host/request2', {
           method: 'POST',
-          body: JSON.stringify({ id: 15 }),
+          body: JSON.stringify({ id: 2 }),
         }),
       )
     }
     fetchData()
   }, [])
 
-  return <div>Ciao!</div>
+  return <div>Hi {name}!</div>
 }
 
 it('should warn about the code making a request that has not being mocked', async () => {
   const consoleWarn = jest.spyOn(console, 'warn').mockImplementation()
 
-  wrap(DummyComponent)
+  wrap(GreetingComponent)
     .withNetwork({
       path: '/request1',
       host: 'my-host',
+      method: 'post',
+      requestBody: { id: 1 },
+      responseBody: { name: 'Joe' },
     })
     .debugRequests()
     .mount()
 
-  await wait(() => {
-    expect(consoleWarn).toHaveBeenCalledWith(
-      expect.stringContaining('cannot find any mock matching:'),
-    )
-    expect(consoleWarn).toHaveBeenCalledWith(
-      expect.stringContaining('URL: my-host/request2'),
-    )
-    expect(consoleWarn).toHaveBeenCalledWith(
-      expect.stringContaining('METHOD: post'),
-    )
-    expect(consoleWarn).toHaveBeenCalledWith(
-      expect.stringContaining('REQUEST BODY: {"id":15}'),
-    )
-  })
+  await screen.findByText('Hi Joe!')
+
+  expect(consoleWarn).toHaveBeenCalledWith(
+    expect.stringContaining('cannot find any mock matching:'),
+  )
+  expect(consoleWarn).toHaveBeenCalledWith(
+    expect.stringContaining('URL: my-host/request2'),
+  )
+  expect(consoleWarn).toHaveBeenCalledWith(
+    expect.stringContaining('METHOD: post'),
+  )
+  expect(consoleWarn).toHaveBeenCalledWith(
+    expect.stringContaining('REQUEST BODY: {"id":2}'),
+  )
 })
 
 it('should not warn if the debugRequests feature is not used', async () => {
   const consoleWarn = jest.spyOn(console, 'warn').mockImplementation()
 
-  wrap(DummyComponent)
+  wrap(GreetingComponent)
     .withNetwork({
       path: '/request1',
       host: 'my-host',
+      method: 'post',
+      requestBody: { id: 1 },
+      responseBody: { name: 'Joe' },
     })
     .mount()
 
-  await wait(() => {
-    expect(consoleWarn).not.toHaveBeenCalledWith(
-      expect.stringContaining('cannot find any mock matching:'),
-    )
-  })
+  await screen.findByText('Hi Joe!')
+
+  expect(consoleWarn).not.toHaveBeenCalledWith(
+    expect.stringContaining('cannot find any mock matching:'),
+  )
 })
 
 it('should not warn if all the requests are being mocked', async () => {
   const consoleWarn = jest.spyOn(console, 'warn').mockImplementation()
 
-  wrap(DummyComponent)
+  wrap(GreetingComponent)
     .withNetwork([
       {
         path: '/request1',
         host: 'my-host',
-        method: 'get',
+        method: 'post',
+        requestBody: { id: 1 },
+        responseBody: { name: 'Joe' },
       },
       {
         path: '/request2',
         host: 'my-host',
         method: 'post',
-        requestBody: { id: 15 },
+        requestBody: { id: 2 },
+        responseBody: { name: 'Sam' },
       },
     ])
     .debugRequests()
     .mount()
 
-  await wait(() => {
-    expect(consoleWarn).not.toHaveBeenCalledWith(
-      expect.stringContaining('cannot find any mock matching:'),
-    )
-  })
+  await screen.findByText('Hi Joe!')
+
+  expect(consoleWarn).not.toHaveBeenCalledWith(
+    expect.stringContaining('cannot find any mock matching:'),
+  )
 })
