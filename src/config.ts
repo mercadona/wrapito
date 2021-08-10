b37d1c249572130321afbd1a201fd2cb3e2c90e5
Raúl Moya Reyes <info@raulmoya.es>
2021-08-10 12:36:10 +0200

    [Added] Typescript models

---
+++ src/config.ts
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Component, Config, Mount } from './models'
+
+const mount = (component: Component) => {
+  const rootNode = document.body.appendChild(document.createElement('div'))
+
+  render(component, rootNode)
+
+  return rootNode
+}
+
+let config: Config = {
+  defaultHost: '',
+  extend: {},
+  mount,
+}
+
+function configure(newConfig: Config) {
+  config = {
+    ...config,
+    ...newConfig,
+  }
+}
+
+const getConfig = (): Config => ({ ...config })
+
+export { configure, getConfig, Config, Mount }
---
+++ src/models.ts
@@ -0,0 +1,54 @@
+import React from 'react'
+
+interface Response {
+  path: string
+  method?: string
+  status?: number
+  host?: string
+  responseBody?: object
+  requestBody?: object
+  multipleResponses?: Response[]
+  catchParams?: boolean
+  delay?: number
+}
+
+interface Wrap {
+  withNetwork: (responses: Response[]) => Wrap
+  atPath: (path: string) => Wrap
+  withProps: (props: object) => Wrap
+  debugRequests: () => Wrap
+  mount: () => object
+}
+
+interface WrapOptions {
+  Component: typeof React.Component
+  responses: Response[]
+  props: object
+  path: string
+  hasPath: boolean
+  debug: boolean
+}
+
+interface WrapExtensionAPI {
+  addResponses: () => void
+}
+
+type Extension = <T>(extensionAPI: WrapExtensionAPI, args: T) => Wrap
+
+type Extensions = {
+  [key: string]: Extension
+}
+
+type Component = React.ReactElement<any, any>
+
+type Mount = (component: Component) => HTMLDivElement
+
+interface Config {
+  defaultHost: string
+  mount: Mount
+  extend: Extensions
+  portal?: string
+  history?: History
+}
+
+export { Wrap, WrapOptions, Response, Config, Mount, Component }
