3dad0176c84d0480616df2fff7cce84436db8a13
David Alfageme <alfalfageme@gmail.com>
2021-09-02 17:18:45 +0200

    [Changed] Warning message

--- src/wrap.tsx
+++ src/wrap.tsx
@@ -107,6 +107,7 @@ const getMount =
 
     if (hasPath && history) {
       console.warn('wrapito WARNING: history is DEPRECATED. Pass a changeRoute function to the config instead.')
+      console.warn('Read about changeRounte in: https://github.com/mercadona/wrapito#changeRoute')
       history.push(path)
     }
 
