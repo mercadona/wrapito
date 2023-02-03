5fd1aaf1402582054f1f19b6e6453947240dc1f8
andreacasasrdrg <andrea_cr_1994@hotmail.com>
2023-02-03 11:02:26 +0100

    axios failed request test

--- tests/withNetwork.xhr.test.js
+++ tests/withNetwork.xhr.test.js
@@ -8,13 +8,15 @@ it('should have network by default', async () => {
   wrap(() => <div></div>).withNetwork([
     { path: '/foo/bar', responseBody: { foo: 'bar' }}
   ]).mount()
-
+  const mockedFn = jest.fn()
   let xhr = new XMLHttpRequest()
   xhr.open('GET', '/foo/bar')
+  xhr.onreadystatechange = mockedFn
   xhr.send()
 
   expect(xhr.response).toEqual({ foo: 'bar' })
   expect(xhr.status).toEqual(200)
+  expect(mockedFn).toHaveBeenCalled()
 })
 
 it('should have network by default', async () => {
@@ -29,4 +31,19 @@ it('should have network by default', async () => {
 
   expect(firstResponse.data).toEqual({ foo: 'bar' })
   expect(secondResponse.data).toEqual({ bar: 'foo' })
+})
+
+it('should handle failed axios requests', async () => {
+  configure({ mount: render })
+  wrap(() => <div></div>).withNetwork([
+    { path: '/bar/foo', responseBody: { bar: 'foo' }, status: 400},
+  ]).mount()
+
+  try {
+    await axios.get('/bar/foo', { responseType: 'application/json' })
+  } catch (error) {
+    expect(error.name).toBe('AxiosError')
+    expect(error.message).toBe('Request failed with status code 400')
+    expect(error.response.data).toEqual({ bar: 'foo' })
+  }
 })
