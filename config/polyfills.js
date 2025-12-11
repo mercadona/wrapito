9dd66fffc03b91ba7af41403734c5655eeb617b7
Arturo Sanchez <artsanch@mercadona.es>
2025-12-11 18:00:19 +0100

    chore: yet another attempt

--- config/polyfills.js
+++
@@ -1,8 +0,0 @@
-import { fetch, Request, Response, Headers } from 'whatwg-fetch'
-
-// Ensure jsdom's window has a fetch implementation before wrapito spies on it.
-global.fetch = global.fetch || fetch
-global.window.fetch = global.window.fetch || global.fetch
-global.Request = Request
-global.Response = Response
-global.Headers = Headers
